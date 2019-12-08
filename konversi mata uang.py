d07ffbd5ad37442adbe49d868122f99df27e409a
Faqihyugos <faqihyugos@gmail.com>
2019-12-08 23:21:17 +0700

    push

---
+++ konversi mata uang.py
@@ -0,0 +1,19 @@
+# title : koversi mata uang
+# Author : faqih yugo susilo
+# description :  KONVERSI MATA UANG UERO, HONGKONG DOLLAR DAN JAPANESE YEN KE RUPIAH
+print('''
+===========================================================================
+    KONVERSI MATA UANG UERO, HONGKONG DOLLAR DAN JAPANESE YEN KE RUPIAH
+===========================================================================''')
+a = int(input("Masukkan Nominal yang ingin di konversi : "))
+e = a*15783
+h = a*1804
+j = a*13088
+print('''===========================================================================
+|   KONVERSI MATA UANG UERO, HONGKONG DOLLAR DAN JAPANESE YEN KE RUPIAH   |
+===========================================================================
+|   ''', a, '''UER ke IDR     |  ''', a, '''HKD ke IDR    |  ''', a, '''JPY ke IDR     |
+===========================================================================
+|    ''', e, '''            |     ''', h, '''        |     ''', j, '''          |
+===========================================================================
+''')
